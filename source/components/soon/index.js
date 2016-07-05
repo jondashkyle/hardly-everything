0d80a86c3751d6a0f21a5b1af5fa3dcf50920953
Jon-Kyle Mohr <contact@jon-kyle.com>
2016-07-04 18:32:27 -0700

    Initial scaffolding and cleanup

---
+++ source/components/soon/index.js
@@ -0,0 +1,16 @@
+const html = require('choo/html')
+
+/**
+ * View
+ */
+exports.view = (state, prev) => {
+  return html`
+    <div class="x xjc xac psf t0 l0 r0 b0">
+      <div class="tac">
+        <span class="h3">
+          🕶
+        </span>
+      </div>
+    </div>
+  `
+}
