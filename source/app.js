ad12feb715a740d9bcef620929c82f162dc2f3b9
Jon-Kyle Mohr <contact@jon-kyle.com>
2016-10-03 19:09:47 -0700

    update

---
+++ source/app.js
@@ -0,0 +1,20 @@
+const choo = require('choo')
+const log = require('choo-log')
+
+const app = choo()
+app.use(log())
+
+app.model(require('./model/user'))
+app.model(require('./model/entries'))
+app.model(require('./model/options'))
+app.model(require('./model/staging'))
+app.model(require('./model/ui'))
+
+app.model(require('./containers/panel-options').model)
+
+app.router((route) => [
+  route('/', require('./templates/index')),
+  route('/data', require('./templates/export'))
+])
+
+module.exports = app
--- source/index.js
+++ source/index.js
@@ -1,36 +1,7 @@
-const choo = require('choo')
+const app = require('./app')
 
 require('./db')
 require('./css')
 
-/**
- * App
- */
-const app = choo()
-const log = require('choo-log')
-app.use(log())
-
-/**
- * Model
- */
-app.model(require('./model/user'))
-app.model(require('./model/entries'))
-app.model(require('./model/options'))
-app.model(require('./model/staging'))
-app.model(require('./model/ui'))
-
-app.model(require('./containers/panel-options').model)
-
-/**
- * Router
- */
-app.router((route) => [
-  route('/', require('./templates/index')),
-  route('/data', require('./templates/export'))
-])
-
-/**
- * Initialize
- */
 const tree = app.start()
 document.body.appendChild(tree)
