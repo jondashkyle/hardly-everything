ce4d45f3bf6644f6f19db8ab2a16c5262e250c03
Jon-Kyle Mohr <contact@jon-kyle.com>
2016-10-05 19:32:20 -0700

    data reset

--- source/templates/reset.js
+++ source/templates/reset.js
@@ -1,15 +1,35 @@
 const h = require('choo/html')
 
+const css = require('../components/css')
+
 const reset = (send) => {
   send('entries:reset')
   send('options:reset')
 }
 
 module.exports = (state, prev, send) => {
-  return h`<div
+  const elContainer = content => h`<div
     class="psf t0 r0 b0 l0 x xjc xac fs2"
-    onload=${el => reset(send)}
-  >
-    Your data has been reset
+  >${content}</div>`
+
+  const elReset = h`
+    <div
+      class="c4 py1 fs2 curp bg-black tc-white tac"
+      onclick=${el => reset(send)}
+    >
+      Reset all data
+    </div>
+  `
+
+  const elConfirmation = h`<div>
+    Your local data has been reset
+  </div>`
+
+  return h`<div> 
+    ${elContainer(Object.keys(state.entries.all).length === 0
+      ? elConfirmation
+      : elReset
+    )}
+    ${css(state, prev, send)}
   </div>`
 }
