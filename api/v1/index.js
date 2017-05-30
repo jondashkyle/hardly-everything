129e29ae04660af1de098867976f8e7050e2ae35
jondashkyle <contact@jon-kyle.com>
2017-05-30 19:38:06 -0400

    testing hooks

--- api/v1/index.js
+++ api/v1/index.js
@@ -14,6 +14,11 @@ module.exports = v1
 
 function v1 (app) {
   app.route('POST', '/v1/hardly-a-hook', handleHook) 
+  app.route('get', '/v1/test', handleTest) 
+}
+
+function handleTest (req, res, ctx) {
+  ctx.send(200, { message: 'it works!' })
 }
 
 function handleHook (req, res, ctx) {
