5e9e2189aa1cc4d48d3320c451a10c77b81b80cf
jondashkyle <contact@jon-kyle.com>
2017-06-04 14:56:17 -0400

    loading spinner fix

--- hello/src/index.js
+++ hello/src/index.js
@@ -3,3 +3,7 @@ require('./styles')
 
 const tree = app.start()
 document.body.appendChild(tree)
+
+// temporary loading fix
+var loader = document.querySelector('[data-load]')
+loader.parentNode.removeChild(loader)
