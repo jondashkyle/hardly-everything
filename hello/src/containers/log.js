f77d78178050d09ead647be22b942e1a48f63433
jondashkyle <contact@jon-kyle.com>
2017-06-04 22:15:29 -0400

    cleanup

--- hello/src/containers/log.js
+++ hello/src/containers/log.js
@@ -4,92 +4,98 @@ var md = require('marked')
 
 var content = require('../content/log')
 
-var formatContent = content => {
+module.exports = view
+
+function formatContent (content) {
   var el = h`<div></div>`
   el.innerHTML = md(content)
   return el
 }
 
-var elList = (entry, send) => h`
-  <div class="p1 x xw" sm="p0">
-    <div class="c4 p1" sm="c11">
-      <div>
-        <a href=${entry.url}>${entry.title}</a>
+function elList (entry, send){
+  return h`
+    <div class="p1 x xw" sm="p0">
+      <div class="c4 p1" sm="c11">
+        <div>
+          <a href=${entry.url}>${entry.title}</a>
+        </div>
+        <div>
+          ${formatDate(entry.date)}
+        </div>
       </div>
-      <div>
-        ${formatDate(entry.date)}
+      <div class="c8 x xw" sm="c12">
+        <div class="p1 ${entry.text ? '' : 'dn'}">
+          ${entry.text}
+        </div>
+        <div class="c4 p1" sm="c2">
+          Day
+        </div>
+        <div class="c8 p1" sm="c10">
+          <div>
+            <a href="http://${entry.day.url}">${entry.day.url}</a>
+          </div>
+          <div>
+            ${entry.day.text}
+          </div>
+        </div>
+        <div class="c4 p1" sm="c2">
+          Week
+        </div>
+        <div class="c8 p1" sm="c10">
+          <div>
+            <a href="http://${entry.week.url}">${entry.week.url}</a>
+          </div>
+          <div>
+            ${entry.week.text}
+          </div>
+        </div>
+        <div class="c4 p1" sm="c2">
+          Month
+        </div>
+        <div class="c8 p1" sm="c10">
+          <div>
+            <a href="http://${entry.month.url}">${entry.month.url}</a>
+          </div>
+          <div>
+            ${entry.month.text}
+          </div>
+        </div>
+        <div class="c4 p1" sm="c2">
+          Century
+        </div>
+        <div class="c8 p1" sm="c10">
+          <div>
+            <a href="http://${entry.century.url}">${entry.century.url}</a>
+          </div>
+          <div>
+            ${entry.century.text}
+          </div>
+        </div>
       </div>
     </div>
-    <div class="c8 x xw" sm="c12">
-      <div class="p1 ${entry.text ? '' : 'dn'}">
-        ${entry.text}
-      </div>
-      <div class="c4 p1" sm="c2">
-        Day
-      </div>
-      <div class="c8 p1" sm="c10">
-        <div>
-          <a href="http://${entry.day.url}">${entry.day.url}</a>
-        </div>
-        <div>
-          ${entry.day.text}
-        </div>
-      </div>
-      <div class="c4 p1" sm="c2">
-        Week
-      </div>
-      <div class="c8 p1" sm="c10">
-        <div>
-          <a href="http://${entry.week.url}">${entry.week.url}</a>
-        </div>
-        <div>
-          ${entry.week.text}
-        </div>
-      </div>
-      <div class="c4 p1" sm="c2">
-        Month
-      </div>
-      <div class="c8 p1" sm="c10">
-        <div>
-          <a href="http://${entry.month.url}">${entry.month.url}</a>
-        </div>
-        <div>
-          ${entry.month.text}
-        </div>
-      </div>
-      <div class="c4 p1" sm="c2">
-        Century
-      </div>
-      <div class="c8 p1" sm="c10">
-        <div>
-          <a href="http://${entry.century.url}">${entry.century.url}</a>
-        </div>
-        <div>
-          ${entry.century.text}
-        </div>
-      </div>
-    </div>
-  </div>
-`
+  `
+}
 
-var elEntry = (entry, send) => h`
-  <div class="p1 x xw">
-    <div class="p1 c4" sm="c12">
-      <div>
-        ${entry.title}
+function elEntry  (entry, send) {
+  return h`
+    <div class="p1 x xw">
+      <div class="p1 c4" sm="c12">
+        <div>
+          ${entry.title}
+        </div>
+        <div>
+          ${formatDate(entry.date)}
+        </div>
       </div>
-      <div>
-        ${formatDate(entry.date)}
+      <div class="p1 c8" sm="c12">
+        ${formatContent(entry.text)}
       </div>
     </div>
-    <div class="p1 c8" sm="c12">
-      ${formatContent(entry.text)}
-    </div>
-  </div>
-`
+  `
+}
 
-var view = (state, prev, send) => {
-  return elsContent = ov(content)
+function view (state, prev, send) {
+  return ov(content)
     .map(function (entry) {
       return h`
         <div class="mb4">
@@ -101,8 +107,6 @@ var view = (state, prev, send) => {
       `
     })
 }
-
-module.exports = view
 
 function formatDate (date) {
   var monthNames = [
