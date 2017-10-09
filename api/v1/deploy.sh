157fd49f73548cbc18d2e4b3db20c5f13b799f88
jondashkyle <contact@jon-kyle.com>
2017-10-08 17:34:27 -0700

    refactor

--- api/v1/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd ../../ && \
-git pull origin master && \
-cd api && npm install && \
-cd ../hello && npm install && npm run build &&
-cd ../app && npm install && npm run build &&
-pm2 restart api
--- documents/17-06-21-braintstorm.md
+++
@@ -1,3 +0,0 @@
-with everyone vying for your attention, you need a private place to remember what matters to you.
-
-a feed of memories.
